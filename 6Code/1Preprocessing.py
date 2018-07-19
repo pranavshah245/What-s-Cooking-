6582b4481ebe2fb8dbc3dbf9cea3742d80339b8c
mwaghela92 <mayur.1992@yahoo.com>
2018-07-18 21:12:04 -0400

    Check1

--- 6Code/1Preprocessing.py
+++ 6Code/1Preprocessing.py
@@ -13,8 +13,11 @@ import json
 
 # %% Setup path and file names
 
-path_RawData = "C:/My Data/Projects/Kaggle/Whats-Cooking/1RawData/"
-path_ProcessedData = "C:/My Data/Projects/Kaggle/Whats-Cooking/2ProcessedData/"
+#path_RawData = "C:/My Data/Projects/Kaggle/Whats-Cooking/1RawData/"
+#path_ProcessedData = "C:/My Data/Projects/Kaggle/Whats-Cooking/2ProcessedData/"
+
+path_RawData = "/Users/mayur/Documents/GitHub/What-s-Cooking-/1RawData/"
+path_ProcessedData = "/Users/mayur/Documents/GitHub/What-s-Cooking-/2ProcessedData/"
 
 filename_Train = "train.json"
 filename_Test = "test.json"
@@ -37,6 +40,8 @@ df_Test = df_Test[["id", "ingredients"]]
 # Sorting the dataframe by ID
 df_Train = df_Train.sort_values(by="id")
 df_Test = df_Test.sort_values(by="id")
+
+df_Train.head()
 
 # Checking for missing values/Imputation if necessarry
 
