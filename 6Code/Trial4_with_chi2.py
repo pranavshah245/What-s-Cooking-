bc6153fac8fe173f65eb6d54c52ecfded4016ece
mwaghela92 <mayur.1992@yahoo.com>
2018-08-05 23:30:07 -0400

    classifier changes

--- 6Code/Trial4_with_chi2.py
+++ 6Code/Trial4_with_chi2.py
@@ -15,17 +15,15 @@ from sklearn.metrics import accuracy_score
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.svm import SVC
+import json
 
-df = pd.read_csv('/Users/mayur/Documents/GitHub/What-s-Cooking-/'
-                      + '2ProcessedData/Train.csv')
+df_train = pd.read_json('/Users/mayur/Documents/GitHub/What-s-Cooking-/1RawData/train.json')
+df_test = pd.read_json('/Users/mayur/Documents/GitHub/What-s-Cooking-/1RawData/test.json')
 
-def generate_text(data):
-	text_data = [" ".join(doc['ingredients']).lower() for doc in data]
-	return text_data 
 
-X_train = generate_text(df)
-X_test = generate_text(X_test)
-  
+df = df_train[["id", "ingredients", "cuisine"]]
+df_Test = df_test[["id", "ingredients"]]
+
 c1 = ['thai', 'cajun_creole', 'greek']
 for i in range(len(c1)):
     df1 = df[df['cuisine']== c1[i]]
@@ -43,76 +41,67 @@ c3 = ['french' , 'chinese' , 'indian' ]
 for i in range(len(c3)):
     df1 = df[df['cuisine']== c3[i]]
     df = pd.concat([df,df1],ignore_index = True)
-   
+
+#i = 1
+X_train = list()
+for i in range(len(df)):
+    X_train.append(' '.join(df['ingredients'][i]))
+    
+y_train = df['cuisine']
+    
 from sklearn.feature_extraction.text import TfidfVectorizer
 tfidf = TfidfVectorizer(binary = True)
-features = tfidf.fit_transform(df.ingredients).toarray()
-labels = df.cuisine
-features.shape
 
 
+#df['ingredients']= df['ingredients'].apply(
+ #       lambda x: ast.literal_eval(x))
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
-X_train, X_test, y_train, y_test = train_test_split(df['ingredients'], 
-                                                    df['cuisine'],  random_state = 0)
+#X_train, X_test, y_train, y_test = train_test_split(df['ingredients'], 
+ #                                                   df['cuisine'],  random_state = 0)
 
+#X_train1 = list()
+#X_train = list(X_train)
 
+#[X_train1.append(' '.join(X_train[a])) for a in range(len(X_train))] 
 #count_vect = CountVectorizer()
 #X_train_counts = count_vect.fit_transform(X_train)
 #tfidf_transformer = TfidfTransformer()
 #X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
 X = tfidf.fit_transform(X_train)
 
-"""
-clf = MultinomialNB(alpha = 1).fit(X_train_tfidf, y_train)
-
-print('size of train data : ' + str(len(X_train)))
-
-y_pred = clf.predict(count_vect.transform(X_test))
-
-print(accuracy_score(y_test, y_pred))
-
-from sklearn.svm import SVC
-clf_svm = SVC()
-clf_svm.fit(X_train_tfidf, y_train)
-
-y_pred = clf_svm.predict(count_vect.transform(X_test))
-
-print(accuracy_score(y_test, y_pred))
+type(X)
 
 
-
-from sklearn.model_selection import cross_val_score
-scores = cross_val_score(clf, X_train_tfidf, y_train, cv=18)
-scores 
-
-from sklearn.model_selection import StratifiedKFold
-skf = StratifiedKFold(n_splits=3)
-"""
 lb = LabelEncoder()
 y_train = lb.fit_transform(y_train)
 
-classifier = SVC(C=100, # penalty parameter, setting it to a larger value 
-	 			 kernel='rbf', # kernel type, rbf working fine here
-	 			 degree=3, # default value, not tuned yet
-	 			 gamma=1, # kernel coefficient, not tuned yet
-	 			 coef0=1, # change to 1 from default value of 0.0
-	 			 shrinking=True, # using shrinking heuristics
-	 			 tol=0.001, # stopping criterion tolerance 
-	      		 probability=False, # no need to enable probability estimates
-	      		 cache_size=200, # 200 MB cache size
-	      		 class_weight=None, # all classes are treated equally 
-	      		 verbose=False, # print the logs 
-	      		 max_iter=-1, # no limit, let it run
-          		 decision_function_shape=None, # will use one vs rest explicitly 
+classifier = SVC(C=100, 
+	 			 kernel='rbf', 
+	 			 coef0=1, 
+	 			 shrinking=True,
+	 			 tol=0.001, 
+	      		 probability=False, 
+	      		 cache_size=200,
+	      		 class_weight=None,
+	      		 verbose=False, 
+	      		 max_iter=-1, 
+          		 decision_function_shape=None,  
           		 random_state=None)
-model = OneVsRestClassifier(classifier, n_jobs=4)
+model = OneVsRestClassifier(classifier)
+
+from sklearn.preprocessing import MinMaxScaler
+scaling = MinMaxScaler(feature_range=(-1,1)).fit(X)
+X = scaling.transform(X)
+
+
+
 type(y_train)
 model.fit(X, y_train)
 
-
+X_test = scaling.transform(X_test)
 y_pred = model.predict(count_vect.transform(X_test))
 
 print(accuracy_score(y_test, y_pred))
