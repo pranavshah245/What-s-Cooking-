f13924dce7f47da8492ed07f8dfd9226ea6f6bf3
mwaghela92 <mayur.1992@yahoo.com>
2018-09-09 23:48:06 -0700

    parameter 3

--- 6Code/Tring_new_method1.py
+++ 6Code/Tring_new_method1.py
@@ -49,7 +49,7 @@ X = tfidf_features(train_text, flag="train")
 X_test = tfidf_features(test_text, flag="test")
 type(X)
 # Label Encoding - Target 
-print ("Label Encode the Target Variable ... ")
+print ("Label Encoding the Target Variable ... ")
 lb = LabelEncoder()
 y = lb.fit_transform(target)
 type(y)
@@ -57,11 +57,11 @@ type(y)
 print ("Train the model ... ")
 classifier = SVC(C=100, # penalty parameter, setting it to a larger value 
 	 			 kernel='rbf', # kernel type, rbf working fine here
-	 			 degree=3, # default value, not tuned yet
+	 			 degree=4, # default value, not tuned yet
 	 			 gamma=1, # kernel coefficient, not tuned yet
 	 			 coef0=1, # change to 1 from default value of 0.0
 	 			 shrinking=True, 
-	 			 tol=0.001, # stopping criterion tolerance 
+	 			 tol=0.002, # stopping criterion tolerance 
 	      		 probability=False, # no need to enable probability estimates
 	      		 cache_size=200, # 200 MB cache size
 	      		 class_weight=None, # all classes are treated equally 
